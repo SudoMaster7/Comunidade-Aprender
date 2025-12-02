363d81ce37bb4c23f1d390d9eb3fe2faf2f7d9b3
SudoMaster7 <leosca08@hotmail.com>
2025-12-02 16:13:39 -0300

    Fix DB path for Vercel

--- config.py
+++ config.py
@@ -2,5 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-key-please-change-in-production'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///site.db'
+    # Use /tmp/site.db for serverless environments (writable)
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:////tmp/site.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
